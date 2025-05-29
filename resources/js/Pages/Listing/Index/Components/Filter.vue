fa21ef147627ef917caac2933009fa57ba8c23f2
Chong Jun Wei <chongjw001160@gmail.com>
2025-05-29 13:06:15 +0800

    Added:  - filter form markup, filter styling

---
+++ resources/js/Pages/Listing/Index/Components/Filter.vue
@@ -0,0 +1,43 @@
+<template>
+  <form action="">
+    <div class="mb-8 mt-4 flex flex-wrap gap-2">
+      <div class="flex flex-nowrap items-center">
+        <input
+          type="text" placeholder="Price from"
+          class="filter-input w-28 mr-0.5"
+        />
+        <input
+          type="text" placeholder="Price To"
+          class="filter-input w-28"
+        />
+      </div>
+
+      <div class="flex flex-nowrap items-center">
+        <select class="filter-input w-28 mr-0.5">
+          <option :value="null">Beds</option>
+          <option v-for="n in 5" :key="n" :value="5">{{ n }}</option>
+          <option>6+</option>
+        </select>
+        <select class="filter-input w-28">
+          <option :value="null">Baths</option>
+          <option v-for="n in 5" :key="n" :value="5">{{ n }}</option>
+          <option>6+</option>
+        </select>
+      </div>
+
+      <div class="flex flex-nowrap items-center">
+        <input
+          type="text" placeholder="Area from"
+          class="filter-input w-28 mr-0.5"
+        />
+        <input
+          type="text" placeholder="Area To"
+          class="filter-input w-28"
+        />
+      </div>
+
+      <button type="submit" class="btn-normal">Filter</button>
+      <button type="reset" class="cursor-pointer">Reset</button>
+    </div>
+  </form>
+</template>
