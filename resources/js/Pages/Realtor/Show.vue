86d28253e93c9c9685711fa1f618c05f9f7265f7
Chong Jun Wei <chongjw001160@gmail.com>
2025-06-03 22:23:38 +0800

    Added:  - Accepting Offers (Single Action Controller & Loading Nested Relations)  - ACCEPTED tag if the offer is sold

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -10,23 +10,24 @@
       </div>
     </Box>
 
-    <div v-else class="md:col-span-7 items-center">
+    <div v-else class="md:col-span-7 flex flex-col gap-4">
       <Offer
         v-for="offer in listing.offers"
         :key="offer.id"
         :offer="offer"
         :listing-price="listing.price"
-        class="mb-4"
       />
     </div>
 
-    <Box class="md:col-span-5">
-      <template #header>Basic Info</template>
-      <Price :price="listing.price" class="text-2xl font-bold" />
+    <div class="md:col-span-5">
+      <Box>
+        <template #header>Basic Info</template>
+        <Price :price="listing.price" class="text-2xl font-bold" />
 
-      <ListingSpaceInfo :listing="listing" class="text-lg" />
-      <ListingAddress :listing="listing" class="text-gray-500" />
-    </Box>
+        <ListingSpaceInfo :listing="listing" class="text-lg" />
+        <ListingAddress :listing="listing" class="text-gray-500" />
+      </Box>
+    </div>
   </section>
 </template>
 
--- resources/js/Pages/Realtor/Show/Components/Offer.vue
+++ resources/js/Pages/Realtor/Show/Components/Offer.vue
@@ -1,6 +1,14 @@
 <template>
   <Box>
-    <template #header>Offer #{{ offer.id }}</template>
+    <template #header>
+      Offer #{{ offer.id }}
+      <span
+        v-if="offer.accepted_at"
+        class="dark:bg-green-900 dark:text-green-200 bg-green-200 text-green-900 p-1 rounded-md uppercase ml-1"
+      >
+        accepted
+      </span>
+    </template>
 
     <section class="flex items-center justify-between">
       <div>
@@ -11,7 +19,7 @@
         </div>
 
         <div class="text-gray-500 text-sm">
-          Made By John Doe
+          Made By {{ offer.bidder.name }}
         </div>
 
         <div class="text-gray-500 text-sm">
@@ -20,8 +28,10 @@
       </div>
       <div>
         <Link
+          v-if="notSold"
+          :href="route('realtor.offer.accept', { offer:offer.id })"
           class="btn-outline text-xs font-medium"
-          as="button"
+          as="button" method="PUT"
         >
           Accept
         </Link>
@@ -45,5 +55,8 @@ const difference = computed(
 )
 const madeOn = computed(
   () => new Date(props.offer.created_at).toDateString(),
+)
+const notSold = computed(
+  () => !props.offer.accepted_at && !props.offer.rejected_at,
 )
 </script>
