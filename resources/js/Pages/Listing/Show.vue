d50be0b12add8f02d97f5f2803cedcf5da173b9f
Chong Jun Wei <chongjw001160@gmail.com>
2025-06-03 17:04:36 +0800

    Added:  - Offer Widget (Adding Widget in Vue)  - Making Offer (Backend)

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -64,6 +64,12 @@
           </div>
         </div>
       </Box>
+
+      <MakeOffer
+        v-if="user"
+        :listing-id="listing.id"
+        :price="listing.price"
+      />
     </div>
   </div>
 </template>
@@ -73,9 +79,11 @@ import ListingAddress from '@/Components/ListingAddress.vue'
 import ListingSpaceInfo from '@/Components/ListingSpaceInfo.vue'
 import Price from '@/Components/Price.vue'
 import Box from '@/Components/UI/Box.vue'
-
-import { ref } from 'vue'
+import MakeOffer from '@/Pages/Listing/Show/Components/MakeOffer.vue'
+import { computed, ref } from 'vue'
 import { useMonthlyPayment } from '@/Composables/useMonthlyPayment'
+import { usePage } from '@inertiajs/vue3'
+
 
 const interestRate = ref(2.5)
 const duration = ref(25)
@@ -86,5 +94,10 @@ const props = defineProps({
 
 const { monthlyPayment, totalPaid, totalInterest } = useMonthlyPayment(
   props.listing.price, interestRate, duration,
+)
+
+const page = usePage()
+const user = computed(
+  () => page.props.user,
 )
 </script>
---
+++ resources/js/Pages/Listing/Show/Components/MakeOffer.vue
@@ -0,0 +1,59 @@
+<template>
+  <Box>
+    <template #header>Make an Offer</template>
+    <div>
+      <form @submit.prevent="MakeOffer">
+        <input v-model.number="form.amount" type="text" class="input" />
+        <input
+          v-model.number="form.amount"
+          type="range" :min="min" :max="max" step="10000"
+          class="slider mt-2 w-full h-4"
+        />
+
+        <button type="submit" class="btn-outline w-full mt-2 text-sm">
+          Make an Offer
+        </button>
+        {{ form.errors.amount }}
+      </form>
+    </div>
+
+    <div class="flex justify-between text-gray-500 mt-2">
+      <div>Difference</div>
+      <div>
+        <Price :price="difference" />
+      </div>
+    </div>
+  </Box>
+</template>
+
+
+<script setup>
+import Price from '@/Components/Price.vue'
+import Box from '@/Components/UI/Box.vue'
+import { useForm } from '@inertiajs/vue3'
+import { computed } from 'vue'
+import { route } from 'ziggy-js'
+
+const props = defineProps({
+  listingId: Number,
+  price: Number,
+})
+const form = useForm({
+  amount: props.price,
+})
+
+const MakeOffer = () => form.post(
+  route('listing.offer.store',
+    { listing: props.listingId },
+  ),
+  {
+    preserveScroll: true,
+    preserveState: true,
+  },
+)
+
+const difference = computed(() => form.amount - props.price)
+const min = computed(() => Math.round(props.price / 2))
+const max = computed(() => Math.round(props.price * 2))
+
+</script>
