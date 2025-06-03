91f968333ea627cc072943e6a4eea06ac7888dc2
Chong Jun Wei <chongjw001160@gmail.com>
2025-06-03 18:33:19 +0800

    Added:  - Offer List Page  - Single Listing View with All the Offers

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -1,3 +1,50 @@
 <template>
-  Hello!
+  <div class="mb-4">
+    <Link :href="route('realtor.listing.index')">← Go back to Listings</Link>
+  </div>
+
+  <section class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+    <Box v-if="!hasOffers" class="flex md:col-span-7 items-center">
+      <div class="w-full text-center font-medium text-gray-500">
+        No Offers
+      </div>
+    </Box>
+
+    <div v-else class="md:col-span-7 items-center">
+      <Offer
+        v-for="offer in listing.offers"
+        :key="offer.id"
+        :offer="offer"
+        :listing-price="listing.price"
+        class="mb-4"
+      />
+    </div>
+
+    <Box class="md:col-span-5">
+      <template #header>Basic Info</template>
+      <Price :price="listing.price" class="text-2xl font-bold" />
+
+      <ListingSpaceInfo :listing="listing" class="text-lg" />
+      <ListingAddress :listing="listing" class="text-gray-500" />
+    </Box>
+  </section>
 </template>
+
+<script setup>
+import ListingAddress from '@/Components/ListingAddress.vue'
+import ListingSpaceInfo from '@/Components/ListingSpaceInfo.vue'
+import Price from '@/Components/Price.vue'
+import Box from '@/Components/UI/Box.vue'
+import { Link } from '@inertiajs/vue3'
+import { computed } from 'vue'
+import Offer from '@/Pages/Realtor/Show/Components/Offer.vue'
+
+const props = defineProps({
+  listing: Object,
+})
+
+const hasOffers = computed(
+  () => props.listing.offers.length,
+)
+
+</script>
