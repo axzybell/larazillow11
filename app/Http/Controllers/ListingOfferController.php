64545369d94738987f90927a61de147668f21f69
Chong Jun Wei <chongjw001160@gmail.com>
2025-06-03 22:57:29 +0800

    Added:  - No Offer Making or Changing of Listings that are Sold

--- app/Http/Controllers/ListingOfferController.php
+++ app/Http/Controllers/ListingOfferController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 class ListingOfferController extends Controller
 {
     public function store(Listing $listing, Request $request) {
+        $this->authorize('view', $listing);
         $listing->offers()->save(
             Offer::make(
                 $request->validate([
